45f72ee97a4af9f5dec7fc2014e0f22b3e661a41
tylerfoggoa <tyler.foggoa@gmail.com>
2024-02-14 12:25:23 -0800

    Inital Commit for main branch

---
+++ IA - Default/Backend/main.java
@@ -0,0 +1,14 @@
+public class main{
+    public static void main(String[] args) {
+        
+    }
+}
+
+
+
+// create html gui - game will run locally
+// host on firebase
+    // learn firebase lmao
+
+// java login code
+    // get user login information and 
