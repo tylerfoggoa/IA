2eff4cdc8d631b6a5a7a94899eeca1fa5bcaf904
tylerfoggoa <tyler.foggoa@gmail.com>
2024-02-16 14:52:13 -0800

    made calendar. yippeee!!!11!!

--- IA/Frontend/Scripts/test.js
+++ IA/Frontend/Scripts/test.js
@@ -1,15 +1,14 @@
 const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
 const d = new Date();
 let monthName = month[d.getMonth()];
-const currentDate = new Date();
-const lastDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 0);
-const daysInMonth = lastDayOfMonth.getDate();
-
-
 document.getElementById("month").innerHTML = monthName;
 
 // Get the ul element where the days will be appended
 const daysList = document.getElementById('days-list');
+
+// Get the number of days in the current month
+const daysInMonth = new Date(d.getFullYear(), d.getMonth() + 1, 0).getDate();
 
 // Loop through days and create li elements
 for (let i = 1; i <= daysInMonth; i++) {
@@ -18,7 +17,7 @@ for (let i = 1; i <= daysInMonth; i++) {
 
     // Check if the current day matches the loop day, add a class to style it
     if (i === d.getDate()) {
-    dayElement.classList.add('current-day');
+        dayElement.classList.add('current-day');
     }
 
     daysList.appendChild(dayElement);
