aadfe8f1567336490515fb3d5157874a2b59f392
tylerfoggoa <tyler.foggoa@gmail.com>
2024-03-06 13:05:17 -0800

    Cleaned Up File System and Finalized Styling for Login/Signup Page

--- IA - Gradle/app/src/main/Frontend/Scripts/calendarFunctionality.js
+++ IA - Gradle/app/src/main/Frontend/Scripts/calendarFunctionality.js
@@ -68,7 +68,7 @@ var btn = document.getElementById("days-list");
 // fetch the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
+// when the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
